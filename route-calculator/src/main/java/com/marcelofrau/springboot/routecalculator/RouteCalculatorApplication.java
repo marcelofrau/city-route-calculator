27d500ecf2fb1964dc1fb73b5b7d2ec82b6c342a
Marcelo Frau <marcelofrau@gmail.com>
2018-12-25 20:10:04 -0200

    Fix on incorrect import due to old library usage

--- route-calculator/src/main/java/com/marcelofrau/springboot/routecalculator/RouteCalculatorApplication.java
+++ route-calculator/src/main/java/com/marcelofrau/springboot/routecalculator/RouteCalculatorApplication.java
@@ -3,8 +3,6 @@ package com.marcelofrau.springboot.routecalculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Bootstrap class to start the main RouteCalculatorApplication
