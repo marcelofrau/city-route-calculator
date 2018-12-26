95e081ba75b266a9a6476c51c21d33f94b052828
Marcelo Frau <marcelofrau@gmail.com>
2018-12-26 12:25:11 -0200

    Adding javadoc to SSLVerificationConfig

--- route-calculator/src/main/java/com/marcelofrau/springboot/routecalculator/configuration/SSLVerificationConfig.java
+++ route-calculator/src/main/java/com/marcelofrau/springboot/routecalculator/configuration/SSLVerificationConfig.java
@@ -6,8 +6,6 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.client.ClientHttpRequestFactorySupplier;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -24,16 +22,35 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 
+/**
+ * This class configures the RestTemplate to use a certificate connection with cities-registry micro-service.
+ *
+ * It has stored the certificate and make sure the cities-registry implementation is using
+ * the same SSL certificate to validate. In this way no service without the valid certificate would be
+ * accepted.
+ *
+ * So to communication happen between route-calculator and cities-registry micro-services are SSL validated
+ * through the certificate.
+ *
+ * This class can be improved in the future to allow only matching hostname to be allowed to connect.
+ */
 @Configuration
 public class SSLVerificationConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(SSLVerificationConfig.class);
 
-    // This RestTemplate is using the certificate that is going to connect to cities-registry micro-service.
+    /**
+     * This RestTemplate is using the certificate that is going to connect to cities-registry micro-service.
+     *
+     * @return A restTemplate that can communicate to cities-registry micro-service
+     * @throws KeyStoreException If any problem reading certificate occur
+     * @throws NoSuchAlgorithmException If any problem reading certificate occur
+     * @throws KeyManagementException If any problem reading certificate occur
+     */
     @Bean
     public RestTemplate getRestTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         final HostnameVerifier allPassVerifier = (String hostname, SSLSession sslSession) -> {
-            return true; // ignoring hostname (TODO: this could be improved)
+            return true; // ignoring hostname (TODO: this could be improved in a production environment)
         };
 
         final SSLContext sslContext;
